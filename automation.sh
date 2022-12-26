4003446c891756e6b39fb9f27aa60a42a7203d7d
Shivakumarchitrala <shivakumar.chitrala@gmail.com>
2022-12-26 17:35:43 +0000

    first commit

---
+++ automation.sh
@@ -0,0 +1,16 @@
+# Automatation Script
+#!/bin/bash
+myname=shiva
+timestamp=$(date '+%d%m%Y-%H%M%S')
+s3_bucket=upgrad-shivakumarbucket
+
+sudo apt-get update -y
+apt-get install apache2 -y
+systemctl status apache2
+service apache2 status
+sudo update-rc.d apache2 enable
+
+tar -cvf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
+
+aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
